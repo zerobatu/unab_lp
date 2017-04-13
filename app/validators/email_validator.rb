2501a625ea492d9602ddf8922822b8033d7a3157
Claudio Alvarado <caa.zerobatu@gmail.com>
2017-04-13 18:53:40 -0300

    - change text

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,7 +1,7 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "No es un email")
+      record.errors[attribute] << (options[:message] || "no es un valido")
     end
   end
 end
